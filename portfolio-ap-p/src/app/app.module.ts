3c37603ba79124d3b6413b133f881315a155d709
Mario30450 <kavayerotigre@hotmail.com>
2022-01-01 23:04:43 -0300

    migrando a angular componentes

--- portfolio-ap-p/src/app/app.module.ts
+++ portfolio-ap-p/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { AboutComponent } from './components/about/about.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { EducationComponent } from './components/education/education.component';
+import { HardsoftskillsComponent } from './components/hardsoftskills/hardsoftskills.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProfileComponent,
+    AboutComponent,
+    ExperienceComponent,
+    EducationComponent,
+    HardsoftskillsComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
