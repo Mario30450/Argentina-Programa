fdff28fff7167d87164d18431b80c89d37b7376b
Mario30450 <kavayerotigre@hotmail.com>
2022-01-08 15:36:45 -0300

    probando router

--- portfolio-ap-p/src/app/app-routing.module.ts
+++ portfolio-ap-p/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditPageComponent } from 'edit-page/edit-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'edit-page', component: EditPageComponent }
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
