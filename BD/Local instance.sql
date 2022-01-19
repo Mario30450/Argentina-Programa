ac54f6871e456f51c178035ba1d3d2e8c44bb16b
Mario30450 <kavayerotigre@hotmail.com>
2022-01-19 11:52:31 -0300

    cargando data

---
+++ BD/Local instance.sql
@@ -0,0 +1,2 @@
+INSERT INTO `acerca de`
+VALUES ('Soy profe de Letras egresado de la UNMdP. Me encanta la música, el cine y viajar. Estoy transformando mi vida y mi profesión al ámbito It.', 'El gran motor del cambio es la tecnología Alvin Toffler',1)
