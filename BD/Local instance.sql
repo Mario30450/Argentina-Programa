ca6cf3d5fbaa38de220512a8b31b0d6a802c2cb2
Mario30450 <kavayerotigre@hotmail.com>
2022-01-28 16:56:19 -0300

    nueva BD ok

--- BD/Local instance.sql
+++ BD/Local instance.sql
@@ -1,2 +1,4 @@
-INSERT INTO `acerca de`
-VALUES ('Soy profe de Letras egresado de la UNMdP. Me encanta la música, el cine y viajar. Estoy transformando mi vida y mi profesión al ámbito It.', 'El gran motor del cambio es la tecnología Alvin Toffler',1)
+insert into proyectos (Id_Proyectos, Fecha, Nombre, Descripcion, Url_foto, Perfil_Id_Perfil)
+value (3,'2022-1-1','Portfolio Argentina programa','Actualmente estoy trabajando con Angular ', 'null', 1);
+
+SELECT * FROM proyectos
